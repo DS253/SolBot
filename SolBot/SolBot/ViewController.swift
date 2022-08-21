1325548d4e568b32a8dedfe1f4673dd2a091f4af
DS253 <34843023+DS253@users.noreply.github.com>
2022-08-21 12:23:15 -0700

    Create Project

---
+++ SolBot/SolBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolBot
+//
+//  Created by Daniel Stewart on 8/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
